475fee2b1b9c69f796fb40b5ed8f54593db9d449
beakhand <cat51_2@hotmail.co.jp>
2019-10-12 23:41:22 +0900

    add Request Cell

---
+++ TranslationSNS/Cell/RequestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequestTableViewCell.swift
+//  TranslationSNS
+//
+//  Created by beakhand on 2019/10/12.
+//  Copyright © 2019 beakhand. All rights reserved.
+//
+
+import UIKit
+
+class RequestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
